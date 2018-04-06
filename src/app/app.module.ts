272e2749fb76b323fcf662539dcc0d8529f73052
RyanDrx <Ryan.Dierickx@gmail.com>
2018-04-06 10:38:18 -0700

    Add flex layout to gird

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { routes } from './modules/routes.module';
 import { MaterialModule } from './modules/material.module';
@@ -24,7 +25,8 @@ import { GridComponent } from './grid/grid.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, { useHash: true }),
+    FlexLayoutModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   providers: [MediaMatcher],
   bootstrap: [AppComponent]
